39ad34e3ea7791dc25247114b9dbb20814a4e3b5
Erlend Hamberg <ehamberg@gmail.com>
2012-01-30 21:35:10 +0100

    Add file showing the conceal features for testing

---
+++ conceal-test.py
@@ -0,0 +1,61 @@
+# conceal test -- lines should match
+
+"""
+if a ≠ None: pass """
+if a is not None: pass
+
+"""
+if b is None: pass """
+if b is None: pass
+
+"""
+if 3 ∈ [1,2,3]: pass """
+if 3 in [1,2,3]: pass
+
+"""
+if 4 ∉ [1,2,3]: pass """
+if 4 not in [1,2,3]: pass
+
+"""
+if a ≡ b: pass """
+if a == b: pass
+
+"""
+√(7) """
+sqrt(7)
+
+"""
+π """
+pi
+
+"""
+map (λ x: x, [1,2,3]) """
+map (lambda x: x, [1,2,3])
+
+"""
+a ∧ b """
+a and b
+
+"""
+a ∨ b """
+a or b
+
+"""
+∑([1,2,3]) """
+sum([1,2,3])
+
+"""
+if a ≠ b: pass """
+if a != b: pass
+
+"""
+if a ≥ b: pass """
+if a >= b: pass
+
+"""
+if a ≤ b: pass """
+if a <= b: pass
+
+"""
+if e ∈ [1,2,3] ∧ ¬a: """
+if e in [1,2,3] and not a:
